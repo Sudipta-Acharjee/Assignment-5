f8c81011542c0e40a31a91b3978f32f5cd312eb2
Sudipta Acharjee <sudiptoacharjee4@gmail.com>
2021-02-08 08:12:39 +0600

    updated

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+function foodDetails(foodId) {
+    console.log(foodId);
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`)
+        .then(res => res.json())
+        .then(data => {
+            displayFoodDetails(data.meals[0]);
+        })
+}
+function displayFoodDetails(meal) {
+    document.getElementById("food-details-display").innerHTML = `
+    <div class="text-center">
+        <img src="${meal.strMealThumb}" class="food-details-image">
+        <h3 class="food-details-title">${meal.strMeal}</h3>
+    </div>
+    `
+    document.getElementById("instruction-display").innerHTML = `
+    <p class="instructions">${meal.strInstructions}</p>
+    `
+    console.log(meal);
+    for (let i = 1; i <= 20; i++) {
+        let ingredient = 'strIngredient' + i;
+        let quantity = 'strMeasure' + i;
+        if (meal[ingredient] === "" || meal[ingredient] == null) {
+            break;
+        }
+    }
+    document.getElementById("food-details").style.display = "block";
+}
 document.getElementById("search-button").addEventListener("click", function () {
     warning(""); // removing warning Text
     document.getElementById("food-details").style.display = 'none';
@@ -20,7 +48,6 @@ document.getElementById("search-button").addEventListener("click", function () {
             })
     }
 })
-
 function processData(meals) {
     // clearing previous search result
     document.getElementById("food-list").innerHTML = "";
@@ -28,7 +55,7 @@ function processData(meals) {
     meals.forEach(meal => {
         const mealDiv = document.createElement("div");
         mealDiv.innerHTML = `
-        <div onclick='mealDetails("${meal.idMeal}")' class="food-card">
+        <div onclick='foodDetails("${meal.idMeal}")' class="food-card">
             <img src="${meal.strMealThumb}" class="food-image">
             <h5 class="food-title">${meal.strMeal}</h5>
         </div>
@@ -36,43 +63,6 @@ function processData(meals) {
         document.getElementById("food-list").appendChild(mealDiv);
     });
 }
-
-function mealDetails(foodId) {
-    console.log(foodId);
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`)
-        .then(res => res.json())
-        .then(data => {
-            displayFoodDetails(data.meals[0]);
-        })
-}
-
-function displayFoodDetails(meal) {
-    document.getElementById("food-details-display").innerHTML = `
-    <div class="text-center">
-        <img src="${meal.strMealThumb}" class="food-details-image">
-        <h3 class="food-details-title">${meal.strMeal}</h3>
-    </div>
-    <div>
-        <h4>  Ingredients</h4>
-         <ul id="ingredient-list">
-
-        </ul>
-    </div>
-    `
-    document.getElementById("instruction-display").innerHTML = `
-    <p class="instructions">${meal.strInstructions}</p>
-    `
-    console.log(meal);
-    for (let i = 1; i <= 20; i++) {
-        let ingredient = 'strIngredient' + i;
-        let quantity = 'strMeasure' + i;
-        if (meal[ingredient] === "" || meal[ingredient] == null) {
-            break;
-        }
-    }
-    document.getElementById("food-details").style.display = "block";
-}
-
 function warning(warningText) {
     document.getElementById("warning-text").innerText = warningText;
 }
